fe657d0511cf5528be20b3364038d2df72387055
uran1anUmbra <nestrada7481@gmail.com>
2024-09-19 13:43:53 -0700

    Big work

---
+++ Unity_Intro_PM/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,201 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody myRB;
+    Camera playerCam;
+
+    Vector2 camRotation;
+
+    public Transform weaponSlot;
+
+    [Header("Player Stats")]
+    public int maxHealth = 5;
+    public int health = 5;
+    public int healthRestore = 1;
+
+    [Header("Weapon Stats")]
+    public GameObject shot;
+    public float shotVel = 0;
+    public int weaponID = -1;
+    public int fireMode = 0;
+    public float fireRate = 0;
+    public float currentClip = 0;
+    public float clipSize = 0;
+    public float maxAmmo = 0;
+    public float currentAmmo = 0;
+    public float reloadAmt = 0;
+    public float bulletLifespan = 0;
+    public bool canFire = true;
+
+    [Header("Movement Settings")]
+    public float speed = 10.0f;
+    public float sprintMultiplier = 2.5f;
+    public bool sprintMode = false;
+    public float jumpHeight = 5.0f;
+    public float groundDetectDistance = 1f;
+
+    [Header("User Settings")]
+    public bool sprintToggleOption = false;
+    public float mouseSensitivity = 2.0f;
+    public float Xsensitivity = 2.0f;
+    public float Ysensitivity = 2.0f;
+    public float camRotationLimit = 90f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody>();
+        playerCam = transform.GetChild(0).GetComponent<Camera>();
+
+        camRotation = Vector2.zero;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camRotation.x += Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        camRotation.y += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+
+        camRotation.y = Mathf.Clamp(camRotation.y, -camRotationLimit, camRotationLimit);
+
+        playerCam.transform.localRotation = Quaternion.AngleAxis(camRotation.y, Vector3.left);
+        transform.localRotation = Quaternion.AngleAxis(camRotation.x, Vector3.up);
+
+        if (Input.GetMouseButton(0) && canFire && currentClip > 0 && weaponID >= 0)
+        {
+            GameObject s = Instantiate(shot, weaponSlot.position, weaponSlot.rotation);
+            s.GetComponent<Rigidbody>().AddForce(playerCam.transform.forward * shotVel);
+            Destroy(s,bulletLifespan);
+
+            canFire = false;
+            currentClip--;
+            StartCoroutine("cooldownFire");
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+            reloadClip();
+
+        Vector3 temp = myRB.velocity;
+
+        float verticalMove = Input.GetAxisRaw("Vertical");
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+
+        if(!sprintToggleOption)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+                sprintMode = true;
+
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+                sprintMode = false;
+        }
+
+        if(sprintToggleOption)
+        {
+            if (Input.GetKey(KeyCode.LeftShift) && verticalMove > 0)
+                sprintMode = true;
+
+            if (verticalMove <= 0)
+                sprintMode = false;
+        }
+
+        temp.x = verticalMove * speed;
+        temp.z = horizontalMove * speed;
+
+        if (sprintMode)
+            temp.x *= sprintMultiplier;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, -transform.up, groundDetectDistance))
+            temp.y = jumpHeight;
+
+        myRB.velocity = (temp.x * transform.forward) + (temp.z * transform.right) + (temp.y * transform.up);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "weapon")
+        {
+            other.gameObject.transform.SetPositionAndRotation(weaponSlot.position, weaponSlot.rotation);
+
+            other.gameObject.transform.SetParent(weaponSlot);
+
+            switch(other.gameObject.name)
+            {
+                case "weapon1":
+                    
+                    weaponID = 0;
+                    shotVel = 10000;
+                    fireMode = 0;
+                    fireRate = 0.25f;
+                    currentClip = 20;
+                    clipSize = 20;
+                    maxAmmo = 400;
+                    currentAmmo = 200;
+                    reloadAmt = 20;
+                    bulletLifespan = 1;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if((health < maxHealth) && collision.gameObject.tag == "healthPickup")
+        {
+            health += healthRestore;
+
+            if (health > maxHealth)
+                health = maxHealth;
+
+            Destroy(collision.gameObject);
+        }
+
+        if ((currentAmmo < maxAmmo) && collision.gameObject.tag == "ammoPickup")
+        {
+            currentAmmo += reloadAmt;
+
+            if (currentAmmo > maxAmmo)
+                currentAmmo = maxAmmo;
+
+            Destroy(collision.gameObject);
+        }
+    }
+
+    public void reloadClip()
+    {
+        if (currentClip >= clipSize)
+            return;
+
+        else
+        {
+            float reloadCount = clipSize - currentClip;
+
+            if (currentAmmo < reloadCount)
+            {
+                currentClip += currentAmmo;
+                currentAmmo = 0;
+                return;
+            }
+
+            else
+            {
+                currentClip += reloadCount;
+                currentAmmo -= reloadCount;
+                return;
+            }
+        }
+    }
+
+    IEnumerator cooldownFire()
+    {
+        yield return new WaitForSeconds(fireRate);
+        canFire = true;
+    }
+}
